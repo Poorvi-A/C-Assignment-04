1624b07924bee22bcacb42fa8a01cf4f835198a9
Poorvi-A <99010282+Poorvi-A@users.noreply.github.com>
2023-03-30 18:23:30 +0530

    Add files via upload

---
+++ assignment/Priorityqueue/Priorityqueue/PriorityQueue.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Priorityqueue
+{
+    class PriorityQueue
+    {
+        private ArrayList array;
+
+        public PriorityQueue()
+        {
+            array = new ArrayList();
+        }
+
+        public void Enqueue(int element)
+        {
+            int idx = 0;
+
+            foreach (int item in array)
+            {
+                if (element < item)
+                {
+                    break;
+                }
+                idx++;
+            }
+            array.Insert(idx, element);
+        }
+
+        public int Dequeue()
+        {
+            if (array.Count == 0)
+            {
+                throw new InvalidOperationException("Queue is empty");
+            }
+            int element = (int)array[0];
+            array.RemoveAt(0);
+
+            return element;
+        }
+
+        public int Peek()
+        {
+            if (array.Count == 0)
+            {
+                throw new InvalidOperationException("queue is empty");
+            }
+
+            return (int)array[0];
+        }
+
+        public bool Contains(int value)
+        {
+            for (int i = 0; i < array.Count; i++)
+            {
+                if (value == (int)array[i])
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public int Size()
+        {
+            return array.Count;
+        }
+
+        public int Center()
+        {
+            if (array.Count == 0)
+            {
+                throw new InvalidOperationException("Queue is empty");
+            }
+            int mid = array.Count / 2;
+            return (int)array[mid];
+
+        }
+
+        public void Reverse()
+        {
+            int left = 0;
+            int right = array.Count - 1;
+
+            while (left < right)
+            {
+                int temp = (int)array[left];
+                array[left] = array[right];
+                array[right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < array.Count; i++)
+            {
+                yield return (int)array[i];
+            }
+        }
+
+        public void Print()
+        {
+            foreach (int value in array)
+            {
+                Console.Write(value + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ assignment/Priorityqueue/Priorityqueue/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Priorityqueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PriorityQueue queue = new PriorityQueue();
+            queue.Enqueue(2);
+            queue.Enqueue(5);
+            queue.Enqueue(10);
+            queue.Enqueue(1);
+            queue.Enqueue(8);
+
+            Console.WriteLine("\nElements inside queue are: ");
+            queue.Print();
+
+            Console.WriteLine("\nPeek element is: " + queue.Peek());
+
+            // print using iterator
+            Console.WriteLine("\nPrint using iterator: ");
+            foreach (int element in queue)
+            {
+                Console.Write(element + " ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("\nContains: " + queue.Contains(10));
+
+            Console.WriteLine("\nElement removed: " + queue.Dequeue());
+            queue.Print();
+
+            Console.WriteLine("\nCenter element is: " + queue.Center());
+
+            Console.WriteLine("\nReverse : ");
+            queue.Reverse();
+            queue.Print();
+
+            Console.ReadLine();
+        }
+    }
+}
